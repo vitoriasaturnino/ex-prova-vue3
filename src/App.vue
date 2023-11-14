0e36df76cf692d57d49d35fe97ebc5f0bc1e3938
Vitória Moura <vim.saturnino@gmail.com>
2023-11-14 13:05:58 +0000

    feat: creat link to page temperaturas

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/temperaturas">Temperaturas</RouterLink>
       </nav>
     </div>
   </header>
